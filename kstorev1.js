fed3b9e4f15ade16149ef60803ae6d9aa544be6c
monshinedeveloper <160322985+monshinedeveloper@users.noreply.github.com>
2024-02-24 21:23:24 +0100

    update kstorev1.js

--- kstorev1.js
+++ kstorev1.js
@@ -2,3 +2,8 @@ function apk() {
     alert('Download APK and install the APK directl on your phone');
     window.location.href='https://drive.google.com/drive/folders/1-mtD6_T988yZ6vASxj3tgUmxo6NQYhgF';
 }
+
+function xapk() {
+    alert('Download first the APK installer from the APK site');
+    
+}
